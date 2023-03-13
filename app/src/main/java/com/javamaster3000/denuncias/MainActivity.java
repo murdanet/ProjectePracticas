19ac6198da06e7cecfb445244dcd40a3aa54cfa7
Mike <murdanet@jaumebalmes.net>
2023-03-13 13:01:04 +0100

    Versión con plantilla de layouts inicial, lunes 13/03

---
+++ app/src/main/java/com/javamaster3000/denuncias/MainActivity.java
@@ -0,0 +1,14 @@
+package com.javamaster3000.denuncias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
